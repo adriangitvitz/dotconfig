974538509a778fda40b51784a40c9a1599c9b83d
Adrian Najera <39295224+adriangitvitz@users.noreply.github.com>
2025-06-05 14:23:36 -0600

    theme switcher

--- lua/dev/plugins/colorscheme.lua
+++ lua/dev/plugins/colorscheme.lua
@@ -1,24 +1,37 @@
--- return {
---     "adriangitvitz/zenburn.nvim",
---     config = function()
---         require("zenburn").setup()
---         vim.cmd("colorscheme zenburn")
---         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
---         vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
---         vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
---         vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
---         vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
---     end
--- }
 return {
-    "adriangitvitz/jellybeans.nvim",
-    branch = "local",
-    lazy = false,
-    priority = 1000,
-    opts = {
-        transparent = true,
+    {
+        "adriangitvitz/jellybeans.nvim",
+        branch = "local",
+        lazy = false,
+        priority = 1000,
+        opts = {
+            transparent = true,
+        },
     },
-    config = function()
-        vim.cmd [[colorscheme jellybeans-mono]]
-    end
+    {
+        "adriangitvitz/zenburn.nvim",
+        config = function()
+            require("zenburn").setup()
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+            vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+            vim.api.nvim_set_hl(0, "Pmenu", { bg = "none" })
+            vim.api.nvim_set_hl(0, "LineNr", { bg = "none" })
+            vim.api.nvim_set_hl(0, "WinSeparator", { bg = "none" })
+        end
+    },
+    {
+        "slugbyte/lackluster.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("lackluster").setup({
+                disable_plugin = {
+                    bufferline = true,
+                },
+                tweak_background = {
+                    normal = "none",
+                }
+            })
+        end
+    }
 }
